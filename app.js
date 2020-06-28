8c46d8620309cceba094688878a0f14ce5cae007
Sam Chillcott <sam@samchillcott.com>
2020-06-28 14:00:13 +0100

    renamed js file

---
+++ app.js
@@ -0,0 +1,9 @@
+firebase
+	.auth()
+	.createUserWithEmailAndPassword(email, password)
+	.catch(function (error) {
+		// Handle Errors here.
+		var errorCode = error.code;
+		var errorMessage = error.message;
+		// ...
+	});
