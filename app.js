44301aabc0e87f5f323507841046a7783571e796
Sam Chillcott <sam@samchillcott.com>
2020-06-30 10:13:06 +0100

    collected email a pword

--- app.js
+++ app.js
@@ -6,13 +6,18 @@ signupForm.addEventListener("submit", (e) => {
 	// get user email
 	const email = signupForm["email"].value;
 	console.log(email);
+	
+	// get user email
+	const password = signupForm["password"].value;
+	console.log(password);
 
-// firebase
-// 	.auth()
-// 	.createUserWithEmailAndPassword(email, password)
-// 	.catch(function (error) {
-// 		// Handle Errors here.
-// 		var errorCode = error.code;
-// 		var errorMessage = error.message;
-// 		// ...
-// 	});
+	firebase
+		.auth()
+		.createUserWithEmailAndPassword(email, password)
+		.catch(function (error) {
+			// Handle Errors here.
+			var errorCode = error.code;
+			var errorMessage = error.message;
+			// ...
+	})
+};
