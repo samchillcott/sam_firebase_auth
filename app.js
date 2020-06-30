b4f19591643c6b26f71894668198d1a66aef6ec8
Sam Chillcott <sam@samchillcott.com>
2020-06-30 10:00:14 +0100

    added queryselector

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-firebase
-	.auth()
-	.createUserWithEmailAndPassword(email, password)
-	.catch(function (error) {
-		// Handle Errors here.
-		var errorCode = error.code;
-		var errorMessage = error.message;
-		// ...
-	});
+const signupForm = document.querySelector("#signup-form");
+
+signupForm.addEventListener("submit", (e) => {
+	e.preventDefault();
+
+	// get user email
+	const email = signupForm["email"].value;
+	console.log(email);
+
+// firebase
+// 	.auth()
+// 	.createUserWithEmailAndPassword(email, password)
+// 	.catch(function (error) {
+// 		// Handle Errors here.
+// 		var errorCode = error.code;
+// 		var errorMessage = error.message;
+// 		// ...
+// 	});
