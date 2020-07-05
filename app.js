c5b74d531989ce2d6c9fefbd8d1a30a21113408f
Sam Chillcott <sam@samchillcott.com>
2020-07-05 12:14:08 +0100

    opens upload page when signed up

--- app.js
+++ app.js
@@ -1,37 +1,40 @@
-// // Sign Up
+// Sign Up
 
-// const signupForm = document.querySelector("#signup-form");
+const signupForm = document.querySelector("#signup-form");
 
-// signupForm.addEventListener("submit", (e) => {
-// 	e.preventDefault();
+signupForm.addEventListener("submit", (e) => {
+	e.preventDefault();
 
-// 	// get user email
-// 	const email = signupForm["email"].value;
-// 	// get user email
-// 	const password = signupForm["password"].value;
+	// get user email
+	const email = signupForm["email"].value;
+	// get user email
+	const password = signupForm["password"].value;
 
-// 	const firebaseCreate = async () => {
-// 		let response = null;
+	const firebaseCreate = async () => {
+		let response = null;
 
-// 		try {
-// 			response = await firebase
-// 				.auth()
-// 				.createUserWithEmailAndPassword(email, password);
+		try {
+			response = await firebase
+				.auth()
+				.createUserWithEmailAndPassword(email, password);
 
-// 			// if the response was successful, let the user know and reset the form
-// 			if (response.additionalUserInfo.isNewUser) {
-// 				signupForm.reset();
-// 				alert("Congrats! New user account added");
-// 			}
-// 		} catch (error) {
-// 			// Handle Errors here.
-// 			let errorCode = error.code;
-// 			let errorMessage = error.message;
-// 			alert(errorMessage);
-// 		}
-// 	};
-// 	firebaseCreate();
-// });
+			// if the response was successful, let the user know and reset the form
+			if (response.additionalUserInfo.isNewUser) {
+				signupForm.reset();
+				alert("Congrats! New user account added");
+			}
+		} catch (error) {
+			// Handle Errors here.
+			let errorCode = error.code;
+			let errorMessage = error.message;
+			alert(errorMessage);
+		}
+	};
+	firebaseCreate();
+	window.location.replace("./upload.html");
+});
+
+// Upload File & Data
 
 const uploadForm = document.querySelector("#upload-form");
 
@@ -44,13 +47,13 @@ uploadForm.addEventListener("submit", (e) => {
 	var storageRef = firebase.storage().ref().child(file.files[0].name);
 
 	function bytesToSize(bytes) {
-		const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB']
-		if (bytes === 0) return 'n/a'
-		const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10)
-		if (i === 0) return `${bytes} ${sizes[i]})`
-		return `${(bytes / (1024 ** i)).toFixed(1)} ${sizes[i]}`
-	  }
-	
+		const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
+		if (bytes === 0) return "n/a";
+		const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10);
+		if (i === 0) return `${bytes} ${sizes[i]})`;
+		return `${(bytes / 1024 ** i).toFixed(1)} ${sizes[i]}`;
+	}
+
 	const fileMetaData = {
 		name: file.files[0].name,
 		size: bytesToSize(file.files[0].size),
